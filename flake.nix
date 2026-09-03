92ce722daf4890fe4b43f2c39cd06e3cb9b140ce
Patrick Aldis <patrick.aldis@obsidian.systems>
2026-09-03 15:21:31 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "daml-identity-docs";
+        buildInputs = [pkgs.mdbook];
+      };
+    };
+}
